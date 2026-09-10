58b7c2eb0be5323706b47373dba8f606df420eb1
aale-hub <abeasruiz@gmail.com>
2026-09-10 18:14:30 +0200

    CV Renombrado site.ts

--- src/data/site.ts
+++ src/data/site.ts
@@ -14,6 +14,6 @@ export const siteConfig = {
     linkedin: "https://www.linkedin.com/in/alejandro-beas-ruiz-925b41346/",
     email: "mailto:abeasruiz@gmail.com",
   },
-  cvUrl: "/cv.pdf",
+  cvUrl: "/Alejandro_Beas.pdf",
   heroStack: ["Java", "PHP", "React", "Node.js", "MySQL", "Git"],
 };
